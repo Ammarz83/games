cf8b3558e103c298c583a53df133543449a55213
Ammarz83~ <sapitovammar@gmail.com>
2022-12-01 17:07:34 +0600

    first commit

---
+++ script.js
@@ -0,0 +1,26 @@
+let y = Math.floor(Math.random() * 10 + 1);
+      
+
+let guess = 1;
+  
+document.querySelector("#submitguess").onclick = function(){
+  
+let x = document.querySelector("#guessField").value;
+
+if(x == y)
+{    
+   alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN "
+           + guess + " GUESS ");
+    
+}
+else if(x > y)  
+{    
+   guess++;
+   alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
+else
+{
+   guess++;
+   alert("OOPS SORRY!! TRY A GREATER NUMBER")
+}
+}
